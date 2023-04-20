5e17b8ec79a695c5dd41e24057647f117d1c1c72
Elvis <66243414+Elvis-1@users.noreply.github.com>
2023-04-20 11:27:37 +0100

    updated

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -13,7 +13,7 @@ class Product extends Model
     protected $fillable = [
 		'name','detail','stock','price','discount'
 	];
-    
+
     public function reviews()
     {
         return $this->hasMany(Review::class);
